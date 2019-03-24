8eb97b1dd78b7c183842cc2557d74d24834c7792
K0walslk1 <vien.corp.bot@gmail.com>
2019-03-24 19:04:19 -0400

    changing status

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,7 @@
 module.exports = async function onReady() {
-    this.user.setPresence({ game: { name: `` } })
+    setInterval(async () => {
+        let status = [`Ajudando ${this.users.size} membros! | l!ajuda`, 'lightdevs.tk | l!ajuda']
+        let random = status[Math.floor(Math.random() * status.length)]
+        this.user.setActivity(random)
+    }, 9000)
 }
